ae3498cfdd92fd55fa8e318e4ba2d109eba29b99
Haris Khan <harisali.khan@thetasp.com>
2021-04-23 11:40:58 +0800

    seeding the FE coding repo into the main git repository

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Seeding the FE repo for the coding challenge
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
