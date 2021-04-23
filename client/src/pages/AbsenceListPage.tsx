96dd083af3789fdc43c6d637103f81ad9aaba485
Haris Khan <harisali.khan@thetasp.com>
2021-04-23 20:44:50 +0800

    added filter panel and cleaned up the styling

--- client/src/pages/AbsenceListPage.tsx
+++ client/src/pages/AbsenceListPage.tsx
@@ -7,6 +7,9 @@ import { useActions } from "../actions";
 import * as AbsenceListActionList from "../actions/absenceList";
 import * as MembersListActionList from "../actions/membersList";
 import axios from 'axios';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 
 export function AbsenceListPage() {
     const classes = useStyles();
@@ -24,12 +27,58 @@ export function AbsenceListPage() {
                 })
         })
 
+    const [vacationType, setVacationType] = React.useState('all');
+
+    const handleChange = (event: any) => {
+        console.log(event.target.value);
+        setVacationType(event.target.value);
+    };
+
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+    const handleDateChange = (date: any) => {
+        setSelectedDate(date);
+    };
+
     return (
         <Grid container className={classes.root}>
-            <Grid item xs={6}>
+            <Grid item xs={4}>
                 <Typography variant="h4" gutterBottom>
                     Absence List
 				</Typography>
+            </Grid>
+            <Grid item xs={8}>
+                <Typography variant="h5" gutterBottom>
+                    Filter List
+				</Typography>
+                <Select className={classes.select}
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={vacationType}
+                    onChange={handleChange}>
+                    <MenuItem value={vacationType}>All</MenuItem>
+                    <MenuItem value='vacation'>Vacation</MenuItem>
+                    <MenuItem value='sickness'>Sickness</MenuItem>
+                    <MenuItem value='other'>Other</MenuItem>
+                </Select>
+                <TextField
+                    id="date"
+                    label="From:"
+                    type="date"
+                    defaultValue="2017-05-24"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+                <TextField
+                    id="date"
+                    label="To:"
+                    type="date"
+                    defaultValue="2017-05-24"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
             </Grid>
             <Grid item xs={12}>
                 <AbsenceTableTable />
@@ -46,6 +95,11 @@ const useStyles = makeStyles((theme: Theme) => ({
             paddingLeft: 15,
             paddingRight: 15,
         },
+    },
+
+    select: {
+        height: '48px',
+        marginRight: '20px',
     },
 
     buttonContainer: {
