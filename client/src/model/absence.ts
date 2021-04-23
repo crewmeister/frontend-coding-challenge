4f1b327a14adc11a6936382c405e6b925c9e24f1
Haris Khan <harisali.khan@thetasp.com>
2021-04-23 17:05:57 +0800

    clean up before initializing members reducer and actions

--- client/src/model/absence.ts
+++ client/src/model/absence.ts
@@ -1,11 +1,25 @@
 export interface Todo {
-    id: number;
+    id: string;
     crewId?: string;
-    text?: string;
+    name?: string;
+    userId?: string;
+    admitterId: string;
+    admitterNote: string;
+    confirmedAt: string;
+    createdAt: string;
+    endDate: string;
+    memberNote: string;
+    rejectedAt: string;
+    startDate: string;
+    type: string;
+}
+
+export interface Members {
+    crewId: string;
+    id?: string;
     image?: string;
     name?: string;
     userId?: string;
-    completed: boolean;
 }
 
 export enum AbsenceListActions {
@@ -16,15 +30,21 @@ export enum AbsenceListActions {
     UNCOMPLETE_ABSENCE = "UNCOMPLETE_ABSENCE",
 }
 
+export enum MembersListActions {
+    ADD_MEMBERS = "ADD_MEMBERS",
+    SET_MEMBERS = "SET_MEMBERS",
+}
 interface TodoActionType<T, P> {
     type: T;
     payload: P;
 }
+export type MembersAction =
+    | TodoActionType<typeof MembersListActions.SET_MEMBERS, Todo[]>
+    | TodoActionType<typeof MembersListActions.ADD_MEMBERS, Todo>;
 
 export type TodoAction =
     | TodoActionType<typeof AbsenceListActions.SET_ABSENCE, Todo[]>
     | TodoActionType<typeof AbsenceListActions.ADD_ABSENCE, Todo>
     | TodoActionType<typeof AbsenceListActions.COMPLETE_ABSENCE, number>
     | TodoActionType<typeof AbsenceListActions.UNCOMPLETE_ABSENCE, number>
-    | TodoActionType<typeof AbsenceListActions.DELETE_ABSENCE, number>
-    ;
+    | TodoActionType<typeof AbsenceListActions.DELETE_ABSENCE, number>;
--- client/src/reducers/absences.ts
+++ client/src/reducers/absences.ts
@@ -11,18 +11,6 @@ export const todoList = createReducer<Todo[]>([], {
     [AbsenceListActions.ADD_ABSENCE](state: Todo[], action: TodoAction) {
         return [...state, action.payload];
     },
-    [AbsenceListActions.COMPLETE_ABSENCE](state: Todo[], action: TodoAction) {
-        // search after todo item with the given id and set completed to true
-        return state.map(t => (t.id === action.payload ? { ...t, completed: true } : t));
-    },
-    [AbsenceListActions.UNCOMPLETE_ABSENCE](state: Todo[], action: TodoAction) {
-        // search after todo item with the given id and set completed to false
-        return state.map(t => (t.id === action.payload ? { ...t, completed: false } : t));
-    },
-    [AbsenceListActions.DELETE_ABSENCE](state: Todo[], action: TodoAction) {
-        // remove all todos with the given id
-        return state.filter(t => t.id !== action.payload);
-    },
     [ConfigActions.PURGE_STATE](state: Todo[], action: TodoAction) {
         return [];
     },
