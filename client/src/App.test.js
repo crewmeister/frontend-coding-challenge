9a956fcf6ac3730dc2b91cfbb003880a1e28ef4c
Haris Khan <harisali.khan@thetasp.com>
2021-04-25 01:30:44 +0800

    restructure test file

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,10 +1,9 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
-
 import React from 'react';
-import { shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+
+configure({ adapter: new Adapter() });
 
 describe('App', () => {
     it('should render correctly in "debug" mode', () => {
