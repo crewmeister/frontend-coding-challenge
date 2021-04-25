c958049b1235be5dadeba90351cd596d8b939031
Haris Khan <harisali.khan@thetasp.com>
2021-04-25 17:46:56 +0800

    cleaned up the structure and the data for Absence table

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -7,14 +7,60 @@ import configureStore from 'redux-mock-store';
 
 import '@testing-library/jest-dom';
 
+const mockAbsenceList = [{
+    "admitterId": null,
+    "admitterNote": "",
+    "confirmedAt": "2020-12-12T18:03:55.000+01:00",
+    "createdAt": "2020-12-12T14:17:01.000+01:00",
+    "crewId": 352,
+    "endDate": "2021-01-13",
+    "id": 2351,
+    "memberNote": "",
+    "rejectedAt": null,
+    "startDate": "2021-01-13",
+    "type": "sickness",
+    "userId": 644
+},
+{
+    "admitterId": null,
+    "admitterNote": "Sorry",
+    "confirmedAt": null,
+    "createdAt": "2021-01-03T17:36:52.000+01:00",
+    "crewId": 352,
+    "endDate": "2021-01-05",
+    "id": 2521,
+    "memberNote": "ganzer tag",
+    "rejectedAt": "2021-01-03T17:39:50.000+01:00",
+    "startDate": "2021-01-05",
+    "type": "vacation",
+    "userId": 649
+},
+];
+
+const mockMemberList = [
+    {
+        "crewId": 352,
+        "id": 709,
+        "image": "https://loremflickr.com/300/400",
+        "name": "Max",
+        "userId": 644
+    },
+    {
+        "crewId": 352,
+        "id": 713,
+        "image": "https://loremflickr.com/300/400",
+        "name": "Ines",
+        "userId": 649
+    },
+];
 
 describe('Test Wrapper', () => {
-    let initialState = { membersList: [], absenceList: [] }
+    const initialState = { membersList: mockMemberList, absenceList: mockAbsenceList }
+    const initialStateEmpty = { membersList: [], absenceList: [] }
     let mockStore = configureStore()
     let store: any;
 
     beforeEach(() => {
-        initialState = { membersList: [], absenceList: [] }
         mockStore = configureStore()
     })
 
@@ -22,20 +68,25 @@ describe('Test Wrapper', () => {
         it('should render correctly in "debug" mode', () => {
             store = mockStore(initialState)
             render(<Provider store={store}><App /></Provider>);
-            // expect(component).toMatchSnapshot();
+            expect(screen.getByText('Please proceed to Absence List screen to view the staff that is absent and their details'))
+                .toHaveTextContent('Please proceed to Absence List screen to view the staff that is absent and their details')
         });
     });
 
     describe('AbsenceTable', () => {
         it('should render correctly in "debug" mode', () => {
-            store = mockStore(initialState)
+            store = mockStore(initialState);
             render(<Provider store={store}><AbsenceTable /></Provider>);
-            // const linkElement = screen.getByText(/learn react/i);
-            // expect(linkElement).toBeInTheDocument();
 
-            // const component = shallow(<AbsenceTable />);
-            // console.log(component.debug());
-            // expect(component).toMatchSnapshot();
+            // console.log(screen);
+            // console.log(screen.debug());
+        });
+    });
+
+    describe('AbsenceTable', () => {
+        it('should render correctly in "debug" mode', () => {
+            store = mockStore(initialStateEmpty);
+            render(<Provider store={store}><AbsenceTable /></Provider>);
         });
     });
 });
