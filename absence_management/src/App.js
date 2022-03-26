d1c107cd4a8e05f3a90ecfca1e78827f48afb59f
Ronali <ronalisenapati70@gmail.com>
2022-03-26 23:31:54 +0530

    Created the folder structure

---
+++ absence_management/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Absentees from './Absentees';
+
+function App() {
+  return (
+    <div className="App">
+      <Absentees/>
+    </div>
+  );
+}
+
+export default App;
+
