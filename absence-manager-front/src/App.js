2bb92eeaa42ef2bbc3cab1d2e2c61b0aecaeb10b
nassim <milednassim95@gmail.com>
2022-12-03 00:10:28 +0100

    change absences table column as constant

--- absence-manager-front/src/App.js
+++ absence-manager-front/src/App.js
@@ -2,53 +2,9 @@ import React, { useCallback, useEffect } from "react";
 import { Table, message } from "antd";
 
 import { useAbsences } from "./hooks/useAbsences";
-import { getColumnDatePickerSearchProps } from "./utils";
+import { absenceTableColumns } from "./utils/constants";
 
 import "./App.css";
-
-const columns = [
-  {
-    title: "Member name",
-    dataIndex: "userName",
-  },
-  {
-    title: "Type of absence",
-    dataIndex: "type",
-    filters: [
-      {
-        text: "Vacation",
-        value: "vacation",
-      },
-      {
-        text: "Sickness",
-        value: "sickness",
-      },
-    ],
-    onFilter: (value, record) => record.type.indexOf(value) === 0,
-  },
-  {
-    title: "Start date",
-    dataIndex: "startDate",
-    width: "10%",
-    ...getColumnDatePickerSearchProps("startDate"),
-  },
-  {
-    title: "End date",
-    dataIndex: "endDate",
-    width: "10%",
-    ...getColumnDatePickerSearchProps("endDate"),
-  },
-  {
-    title: "Member note",
-    dataIndex: "memberNote",
-    width: "40%",
-  },
-  {
-    title: "Status",
-    dataIndex: "status",
-  },
-  { title: "Admitter note", dataIndex: "admitterNote" },
-];
 
 const App = () => {
   const [messageApi, contextHolder] = message.useMessage();
@@ -71,7 +27,11 @@ const App = () => {
     <>
       {contextHolder}
       <div className="app">
-        <Table columns={columns} dataSource={data} loading={isLoading} />
+        <Table
+          columns={absenceTableColumns}
+          dataSource={data}
+          loading={isLoading}
+        />
       </div>
     </>
   );
--- absence-manager-front/src/utils/constants.js
+++ absence-manager-front/src/utils/constants.js
@@ -1,5 +1,51 @@
+import { getColumnDatePickerSearchProps } from "./index";
+
 export const absenceTypes = {
   REQUESTED: "Requested",
   CONFIRMED: "Confirmed",
   REJECTED: "Rejected",
 };
+
+export const absenceTableColumns = [
+  {
+    title: "Member name",
+    dataIndex: "userName",
+  },
+  {
+    title: "Type of absence",
+    dataIndex: "type",
+    filters: [
+      {
+        text: "Vacation",
+        value: "vacation",
+      },
+      {
+        text: "Sickness",
+        value: "sickness",
+      },
+    ],
+    onFilter: (value, record) => record.type.indexOf(value) === 0,
+  },
+  {
+    title: "Start date",
+    dataIndex: "startDate",
+    width: "10%",
+    ...getColumnDatePickerSearchProps("startDate"),
+  },
+  {
+    title: "End date",
+    dataIndex: "endDate",
+    width: "10%",
+    ...getColumnDatePickerSearchProps("endDate"),
+  },
+  {
+    title: "Member note",
+    dataIndex: "memberNote",
+    width: "40%",
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
+  },
+  { title: "Admitter note", dataIndex: "admitterNote" },
+];
